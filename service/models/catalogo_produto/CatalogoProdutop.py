044c746a02a47b5f6c148f1b9a67ee79b5e330f8
raynan222 <38507894+raynan222@users.noreply.github.com>
2021-10-01 17:51:34 -0300

    Criação das bases das schemas
    base schema.

---
+++ service/models/catalogo_produto/CatalogoProdutop.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel
+
+Base = declarative_base()
+
+class CatalogoProduto(Base):
+    __tablename__ = 'catalogo_produto'
+    id = Column(Integer, primary_key=True, nullable=False)
+
+class CatalogoProdutoModel(BaseModel):
+    id: int
---
+++ service/models/integracao/Integracao.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel
+
+Base = declarative_base()
+
+class Integracao(Base):
+    __tablename__ = 'integracao'
+    id = Column(Integer, primary_key=True, nullable=False)
+
+class IntegracaoModel(BaseModel):
+    id: int
