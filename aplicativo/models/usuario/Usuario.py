899a03304a3b0fc77b385034659a568aada1e11f
Joel <joelvictor1746@gmail.com>
2021-10-04 20:33:50 -0300

    WIP - primeiro modelo e testes de migracao com alembic

--- aplicativo/models/usuario/Usuario.py
+++ aplicativo/models/usuario/Usuario.py
@@ -1,12 +1,23 @@
-from sqlalchemy import Column, Integer
-from pydantic import BaseModel
+from sqlalchemy import Column, BigInteger, Integer, String
+from pydantic import BaseModel, constr
 from aplicativo.database import Base
+
+# https://docs.sqlalchemy.org/en/14/core/type_basics.html
+# https://pydantic-docs.helpmanual.io/usage/types/
 
 
 class Usuario(Base):
     __tablename__ = "usuario"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    nome = Column(String(255))
+    email = Column(String(255), unique=True)
+    senha = Column(String(50))
+    grupo_id = Column(Integer)
 
 
 class UsuarioModel(BaseModel):
     id: int
+    nome: constr(max_length=255)
+    email: constr(max_length=255)
+    senha: constr(max_length=50)
+    grupo_id = int
