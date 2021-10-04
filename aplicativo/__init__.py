1231c44f83c4269cd6224d772774533543de4c42
Joel <joelvictor1746@gmail.com>
2021-10-04 19:50:42 -0300

    WIP - configuração do banco de dados

--- aplicativo/__init__.py
+++ aplicativo/__init__.py
@@ -1,16 +1,23 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask, _app_ctx_stack
+from flask_cors import CORS
 import infra.config as config
+from sqlalchemy.orm import scoped_session
+from aplicativo.database import SessionLocal, engine, Base
 
 app = Flask(__name__)
 app.config.from_object(config)
 
+CORS(app)  # se o deploy for em vpc ou de msm origem, remover cors
 
-db = SQLAlchemy(app)
+app.session = scoped_session(SessionLocal, scopefunc=_app_ctx_stack.__ident_func__)
 
 
-migrate = Migrate(app, db)
-
-import aplicativo.models
 import aplicativo.routes
+import aplicativo.models
+
+Base.metadata.create_all(bind=engine)
+
+
+@app.teardown_appcontext
+def close_session():
+    app.session.remove()
--- aplicativo/models/auditoria/Auditoria.py
+++ aplicativo/models/auditoria/Auditoria.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, Integer
 from pydantic import BaseModel, constr
-from aplicativo import db
+
+from aplicativo.database import Base
 
 
-class Auditoria(db.Model):
+class Auditoria(Base):
     __tablename__ = "auditoria"
     id = Column(Integer, primary_key=True, nullable=False)
 
