b044006b0310cfb36a3a609b73a200b0ade17c0f
Joel <JoelVIctor1746@gmail.com>
2021-10-04 18:45:49 -0300

    Melhorias na infra basica (#7)

--- service/__init__.py
+++ service/__init__.py
@@ -4,19 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
-
-def config_db():
-    load_dotenv()
-    return os.getenv(
-        "DATABASE", "postgresql://postgres:admin@localhost/banco_teste"
-    )
-
+import infra.config as config
 
 app = Flask(__name__)
-# app.config.from_object("infra/config")
-app.config["SQLALCHEMY_DATABASE_URI"] = config_db()
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-
+app.config.from_object(config)
 
 
 db = SQLAlchemy(app)
@@ -24,7 +15,11 @@ migrate = Migrate(app, db)
 
 import service.models
 
+
+
+
+
 @app.route("/")
 def index():
     print("q")
-    return ujson.dumps("index")
+    return ujson.dumps("meu chapadadadddd")
