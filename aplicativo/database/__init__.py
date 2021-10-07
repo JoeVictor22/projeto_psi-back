28602920df1a7330f66893bcee39aae43f653aeb
Joel <joelvictor1746@gmail.com>
2021-10-06 21:50:57 -0300

    WIP - modelo base para rotas e decorators

--- aplicativo/database/__init__.py
+++ aplicativo/database/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import infra.config as config
 
-engine = create_engine(url=config.SQLALCHEMY_DATABASE_URI)
+engine = create_engine(url=config.SQLALCHEMY_DATABASE_URI, future=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- aplicativo/routes/usuario/resources.py
+++ aplicativo/routes/usuario/resources.py
@@ -1,25 +1,91 @@
+from flask import request
 import ujson
 from aplicativo import app
+from aplicativo.components.routes import field_validator, checar_acesso
 from aplicativo.models.usuario import Usuario, UsuarioModel
-from sqlalchemy import select
+from sqlalchemy import select, insert, update, delete
 from pprint import pprint
 
 prefix = "/usuario"
 
+
 @app.route(f"{prefix}/all", methods=["GET"])
+@checar_acesso("usuario-get")
 def usuario_all():
 
-    # select(Usuario)
-    # result1 = app.session.execute(select).scalars().all()
-    # result2 = app.session.execute(select).all()
-    #
-    # pprint(result1)
-    # pprint(result2)
+    a = select(Usuario)
+    result = app.session.execute(a).scalars().all()
+    pprint(result)
+    result = list(map(UsuarioModel.from_orm, result))
 
-    result1 = app.session.query(Usuario).all()
-    result = list(map(UsuarioModel.from_orm, result1))
-
+    print("\n")
     pprint(result)
 
     return ujson.dumps("b"), 200
 
+
+@app.route(f"{prefix}/view/<item_id>", methods=["GET"])
+@checar_acesso("usuario-get")
+def usuario_get(item_id):
+    result = app.session.get(Usuario, item_id)
+    print("\n")
+    pprint(result)
+
+    if not result:
+        ujson.dumps("fail"), 200
+
+    return ujson.dumps("success"), 200
+
+
+@app.route(f"{prefix}/add", methods=["POST"])
+@checar_acesso("usuario-post")
+@field_validator(UsuarioModel)
+def usuario_add():
+    json = request.get_json()
+    novo_registro = Usuario.from_json(json)
+
+    stmt = insert(Usuario).values(**novo_registro)
+
+    try:
+        app.session.execute(stmt)
+        message = "sucesso"
+    except Exception as e:
+        print(e)
+        message = "falha"
+
+    return ujson.dumps(message), 200
+
+
+@app.route(f"{prefix}/edit/<item_id>", methods=["PUT"])
+@checar_acesso("usuario-put")
+@field_validator(UsuarioModel)
+def usuario_edit(item_id):
+    json = request.get_json()
+
+    dados_alterados = Usuario.from_json(json).to_dict()
+
+    stmt = update(Usuario).where(Usuario.id == item_id).values(**dados_alterados)
+
+    try:
+        app.session.execute(stmt)
+        message = "sucesso"
+    except Exception as e:
+        print(e)
+        message = "falha"
+
+    return ujson.dumps(message), 200
+
+
+@app.route(f"{prefix}/edit/<item_id>", methods=["delete"])
+@checar_acesso("usuario-delete")
+def usuario_delete(item_id):
+    stmt = delete(Usuario).where(Usuario.id == item_id)
+
+    try:
+        app.session.execute(stmt)
+        message = "sucesso"
+    except Exception as e:
+        print(e)
+        message = "falha"
+
+    return ujson.dumps(message), 200
